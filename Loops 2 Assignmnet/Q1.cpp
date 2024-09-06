69840ddf6726858e41695d998e1266970be54b2c
Ayan092001 <ayand9332@gmail.com>
2024-09-06 13:32:28 +0530

    .

---
+++ Loops 2 Assignmnet/Q1.cpp
@@ -0,0 +1,11 @@
+//Predict the output
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    while ('1' < '2')
+        cout << "In while loop" << endl;
+}
+
+//Solution: It will start a infinite loop
---
+++ Loops 2 Assignmnet/Q3.cpp
@@ -0,0 +1,14 @@
+// Predict the output
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for (int x = 1; x * x <= 10; x++)
+        cout << "In for loop" << endl;
+}
+
+// Solution:
+// In for loop
+// In for loop
+// In for loop
