5abb869b67dde8eb48f8fc0373dd65582a21be4d
Ayan092001 <ayand9332@gmail.com>
2024-09-06 22:23:38 +0530

    .

---
+++ Time and space complexity Assignment - 2/Q4.cpp
@@ -0,0 +1,12 @@
+// Calculate the time complexity for the following code snippet.
+
+// int c = 0;
+// for (int i = n; i > 0; i /= 2)
+// {
+//     for (int j = 0; j < i; j++)
+//     {
+//         c++;
+//     }
+// }
+
+// Ans: O(2n) ~ O(n)
