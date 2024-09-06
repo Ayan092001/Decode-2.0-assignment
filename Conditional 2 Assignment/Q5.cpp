bdb96700dbaf6aaa43e899bf7103436d7f35478d
Ayan092001 <ayand9332@gmail.com>
2024-09-06 13:15:29 +0530

    .

---
+++ Conditional 2 Assignment/Q5.cpp
@@ -0,0 +1,14 @@
+// predict the output:
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 18;
+    int b = 12;
+    bool t = (a > 20 && b < 15) ? true : false;
+    cout << "Value of t: " << t;
+    return 0;
+}
+
+//Solution Output : Value of t : 0
