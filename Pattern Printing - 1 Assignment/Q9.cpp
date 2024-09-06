653a14fbadf2b4738e6c9c8838d36070a65449ff
Ayan092001 <ayand9332@gmail.com>
2024-09-06 14:05:59 +0530

    .

---
+++ Pattern Printing - 1 Assignment/Q9.cpp
@@ -0,0 +1,28 @@
+// Print the following pattern
+// Input : n = 4
+// Output :
+//       A
+//     A B
+//   A B C
+// A B C D
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n - i - 1; ++j)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i + 1; ++j)
+        {
+            cout << (char)('A' + j - 1) << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
